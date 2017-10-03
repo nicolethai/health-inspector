a81952e22104ce1458ee107592a0e07f5377d104
nicolethai <ThaiNicole@gmail.com>
2017-10-02 20:58:43 -0700

    Got some test React in

--- src/index.js
+++ src/index.js
@@ -3,4 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+// first component
+const App = () => { // function declaration in Modern JS
+
+    return (
+        // Renders with JSX can only return a single, top-lvl element --> make a parent/top-lvl div
+        <div>
+            <h1>Hello World!</h1>
+                <p>UHHHHHH???</p>
+        </div>
+    ) // return() is a JSX thing, not JS
+}
+// links component to html
+ReactDOM.render(<App />, document.getElementById('root')); // component name
+
+
+
 registerServiceWorker();
